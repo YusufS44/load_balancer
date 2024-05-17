694ea31273ff3e5c5ed20b151f9c29732318751f
YusufS44 <yusuf.sadruddin@gmail.com>
2024-05-16 20:39:52 -0400

    first commit

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.29.1"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = var.project
+  region = var.region
+  credentials = file(var.credentials)
+}
